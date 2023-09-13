907865d7abf458f7bb429b383b1ba3123abd6b6b
ArieTzvibel <a0548463928@gmail.com>
2023-09-13 15:58:47 +0100

    Handling functions on the server.

--- Server/PrintingManager.h
+++ Server/PrintingManager.h
@@ -6,23 +6,7 @@
 
 #include "Header.h"
 #include "Globals.h"
-/*Server*/
 #include"server_manager.h"
-
-
-// int printingSimilarCustomers(PNode node, int flag);
-// int printingClientsInDescendingOrder(PNode root, int flag);//Printing clients from sorted tree froms small to large
-// int printingClientsInAscendingOrder(PNode root, int flag);//Printing clients from sorted tree from big to small 
-// void printInstructions();//Printing instructions for the program.
-// void printTableHead(char* print);//Creating a printable table header
-// void printCell(PClient cell);//Single client printing
-// void printingDebtorsOnly(PClient head);//Printing a list of debts only.
-// void printListDebtsFromTheSmallestToTheLargest(PClient head, char* mainTitle);//Printing the list from the smallest debt to the largest
-// void printingCellsWithErrors(PClient head);//Client printing with errors
-// void printErrors(char* error, int* printsThatWere);//Print the error
-// void printASortedCell(PClient previousList, int flag);//Print a sorted cell
-
-/*Server*/
 void print_instructions();//Printing instructions for the program.
 void print_table_head(char** buffer, char* print);//Creating a printable table header
 void print_cell(PClient cell, char** buffer);//Single client printing
